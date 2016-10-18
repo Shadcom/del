b31ff2c47c9806f1b655bf773761cd1ff655edcc
Dima Lenkov <bagjino@gmail.com>
2016-10-18 10:01:59 +0300

    1111

---
+++ js/common.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+	// Custom JS
+	$(".aside-nav li").click(function() {
+		$(".aside-nav li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".top-mnu li").click(function() {
+		$(".top-mnu li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	// Owl Carousel
+
+	var owl = $("#owl-demo");
+
+	owl.owlCarousel({
+ 
+      
+	  pagination : false,
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+	  autoPlay: true,
+	  itemsMobile : false
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 	  });
+
+	   // Custom Navigation Events
+	$(".next").click(function(){
+    owl.trigger('owl.next');
+  	})
+  	$(".prev").click(function(){
+    owl.trigger('owl.prev');
+  	})
+
+// выравнивание групп товаров
+
+	function setEqualHeight(columns) { 
+		var tallestcolumn = 0; 
+		columns.each( function() { 
+			currentHeight = $(this).height(); 
+			if(currentHeight > tallestcolumn) { tallestcolumn = currentHeight; } } ); 
+			columns.height(tallestcolumn); 
+	} 
+		
+	setEqualHeight($(".cat-name")); 
+	
+
+});
